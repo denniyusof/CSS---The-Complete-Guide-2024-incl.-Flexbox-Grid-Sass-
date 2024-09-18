dc332c8b82f3d1e9ba7a7206b0504f02d7488f06
denniyusof <134164682+denniyusof@users.noreply.github.com>
2024-09-18 18:46:38 +0800

    finish chapter 138

--- section-9/chapter-136/shared.js
+++ section-9/chapter-136/shared.js
@@ -8,8 +8,11 @@ var mobileNav = document.querySelector(".mobile-nav");
 // console.dir(backdrop);
 for (var i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener("click", function () {
-    modal.style.display = "block";
-    backdrop.style.display = "block";
+    // modal.style.display = "block";
+    // backdrop.style.display = "block";
+    // modal.className = 'open'; // this will overwrite the complete class list
+    modal.classList.add("open");
+    backdrop.classList.add("open");
   });
 }
 
@@ -21,23 +24,32 @@ for (var i = 0; i < selectPlanButtons.length; i++) {
 // }
 
 backdrop.addEventListener("click", function () {
-  mobileNav.style.display = "none";
-  backdrop.style.display = "none";
-  modal.style.display = "none";
+  // mobileNav.style.display = "none";
+  // backdrop.style.display = "none";
+  // modal.style.display = "none";
+  backdrop.classList.remove("open");
+  modal.classList.remove("open");
+  mobileNav.classList.remove("open");
 });
 
 modalActionNo.addEventListener("click", function () {
-  backdrop.style.display = "none";
-  modal.style.display = "none";
+  // backdrop.style.display = "none";
+  // modal.style.display = "none";
+  backdrop.classList.remove("open");
+  modal.classList.remove("open");
 });
 
 toggleButton.addEventListener("click", function () {
-  mobileNav.style.display = "block";
-  backdrop.style.display = "block";
+  // mobileNav.style.display = "block";
+  // backdrop.style.display = "block";
+  backdrop.classList.add("open");
+  mobileNav.classList.add("open");
 });
 
 // custom function
 function closeModal() {
   backdrop.style.display = "none";
   modal.style.display = "none";
+  // backdrop.classList.remove("open");
+  // modal.classList.remove("open");
 }
